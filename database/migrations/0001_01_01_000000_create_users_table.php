3e9e4cfbb75da3e9fec462d5a6d7dc87f0cc7d25
KrisnaKintaro <krisnakintaro1256@gmail.com>
2026-03-30 10:58:20 +0700

    Update Migration untuk table user (menambahkan role editor dan redaksi), dan berita (menambahkan status_berita Rejected))

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['Admin', 'Viewer'])->default('Viewer');
+            $table->enum('role', ['Admin', 'Viewer', 'Editor', 'Redaksi'])->default('Viewer');
             $table->rememberToken();
             $table->timestamps();
         });
