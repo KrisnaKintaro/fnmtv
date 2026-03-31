c4ac36dd6ed2c191b14411ed4c5881cae7df4808
KrisnaKintaro <krisnakintaro1256@gmail.com>
2026-03-31 13:58:34 +0700

    Buat CRUD sederhana untuk manajemen kategori

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,23 @@ use App\Http\Controllers\Admin\KategoriController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Route::prefix('admin/manajemen_kategori')->group(function() {
     Route::get('/ambilData',[KategoriController::class, 'getDaftarKategori']);
     Route::post('/tambahData', [KategoriController::class, 'tambahKategoriBaru']);
